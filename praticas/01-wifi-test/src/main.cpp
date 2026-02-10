98fb7cd1103a75824b4145d78164cd258e09e3b4
Marcos Gabriel <marcosgabriel166@gmail.com>
2026-02-09 21:00:05 -0300

    :sparkles: feat: implementa lógica do scanner wifi
    - Nome das redes
    - Sinal (dBm)
    - Segurança

--- praticas/01-wifi-test/src/main.cpp
+++ praticas/01-wifi-test/src/main.cpp
@@ -1,18 +1,62 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include <WiFi.h>
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  unsigned long t = millis();
+  while (!Serial && millis() - t < 2000);
+
+  Serial.println("\n Iniciando WiFi");
+
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  Serial.println("Iniciando a busca...");
+}
+
+String traduzirEncriptacao(wifi_auth_mode_t encryptionType) {
+  switch (encryptionType) {
+    case WIFI_AUTH_OPEN: return "Aberta";
+    case WIFI_AUTH_WEP: return "WEP";
+    case WIFI_AUTH_WPA_PSK: return "WPA";
+    case WIFI_AUTH_WPA2_PSK: return "WPA2";
+    case WIFI_AUTH_WPA_WPA2_PSK: return "WPA/WPA2";
+    case WIFI_AUTH_WPA3_PSK: return "WPA3";
+    case WIFI_AUTH_WPA2_ENTERPRISE: return "WPA2-E";
+    default: return "Desconhecido";
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  Serial.println("\n Escaneando redes..");
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  int n = WiFi.scanNetworks();
+
+  Serial.println("Busca finalizada");
+
+  if (n == 0) {
+    Serial.println("Nenhuma rede encontrada");
+  } else {
+    Serial.printf("%d redes encontradas:\n\n", n);
+
+    Serial.println("   | SSID (Nome)                    | Sinal (dBm) | Segurança    |");
+    Serial.println("   |--------------------------------|-------------|--------------|");
+
+    for (int i = 0; i < n; ++i) {
+      String ssid = WiFi.SSID(i);
+      int32_t rssi = WiFi.RSSI(i);
+      String seguranca = traduzirEncriptacao(WiFi.encryptionType(i));
+      Serial.printf("   | %-30s | %4d dBm   | %-12s |\n", 
+                    ssid.c_str(), 
+                    rssi, 
+                    seguranca.c_str());
+      
+      delay(10);
+    }
+    Serial.println("   --------------------------------------------------------------");
+  }
+  WiFi.scanDelete();
+
+  delay(5000);
 }
